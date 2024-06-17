283d8bd1e4c6c78fdcc3e64e56802ae2e4901fcb
Steven Lacks <snlacks@gmail.com>
2024-06-17 11:03:49 -0400

    change file loading methodology

--- src/mail/connect.service.ts
+++ src/mail/connect.service.ts
@@ -1,18 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { google } from 'googleapis';
 import * as MailComposer from 'nodemailer/lib/mail-composer';
-import * as credentials from '../../_gmail_credentials.json';
-import * as tokens from '../../_gmail_token.json';
+import * as path from 'path';
+import * as fs from 'fs/promises';
+
+export const TOKEN_PATH = path.join(process.cwd(), '_gmail_token.json');
+export const CREDENTIALS_PATH = path.join(
+  process.cwd(),
+  '_gmail_credentials.json',
+);
 
 @Injectable()
 export default class ConnectService {
   getGmailService = async () => {
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    // @ts-ignore
+    const credentials = JSON.parse(content);
     const { client_secret, client_id, redirect_uris } = credentials?.web;
     const oAuth2Client = new google.auth.OAuth2(
       client_id,
       client_secret,
       redirect_uris[0],
     );
+
+    const tokenContent = await fs.readFile(TOKEN_PATH);
+    // @ts-ignore
+    const tokens = JSON.parse(tokenContent);
     oAuth2Client.setCredentials(tokens);
     const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
     return gmail;
